8732a8c72a03318f708c23de0b796212911c15c0
secure-revert-8990 <yuliyambg@gmail.com>
2020-07-30 00:06:49 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,11 @@
+def reverse_each_word(str)
+  arr = str.split(" ")
+  reversed_arr = arr.each {|n| n.reverse!}
+  return reversed_arr.join(" ")
+end
+
+def reverse_each_word(str)
+  arr = str.split(" ")
+  reversed_arr = arr.collect {|n| n.reverse!}
+  return reversed_arr.join(" ")
+end

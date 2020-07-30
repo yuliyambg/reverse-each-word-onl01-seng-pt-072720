d4c52df918afbfaf61458dba34d5e838fe635679
secure-revert-8990 <yuliyambg@gmail.com>
2020-07-30 00:05:14 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,3 +3,8 @@ def reverse_each_word(str)
   reversed_arr = arr.each {|n| n.reverse!}
   return reversed_arr.join(" ")
 end
+
+def reverse_each_word(str)
+  arr = str.split(" ")
+  reversed_arr = arr.collect {|n| n.reverse!}
+  re
